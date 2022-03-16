9bdff7b512204c412404aa18b78915e44c0abd6f
saquib-deloitte <MOHAMSAQUIB@deloitte.com>
2022-03-16 17:21:32 +0530

    pattern

--- pattern.java
+++ pattern.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class pattern {
     public static void main(String[] args) {
-        System.out.println("jgroj");
+        int i, j, k, r;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no of rows you want to print: ");
+        r = sc.nextInt();
+        for (i = 1; i <= r; i++) {
+            for (j = i; j < r; j++) {
+                System.out.print(" ");
+            }
+            for (k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (i = r; i >= 1; i--) {
+            for (j = i; j <= r5; j++) {
+                System.out.print(" ");
+            }
+            for (k = 1; k < i; k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
